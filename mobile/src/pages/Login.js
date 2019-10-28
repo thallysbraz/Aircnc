1561978d90af91648339f68a6b1b0b885282bfd7
thallysbraz <thallysbraz3@gmail.com>
2019-10-28 19:12:40 -0300

    subindo projeto mobile

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Image } from "react-native";
+
+import logo from "../assets/logo.png";
+
+export default function Login() {
+  return (
+    <View>
+      <Image source={logo} />
+    </View>
+  );
+}
